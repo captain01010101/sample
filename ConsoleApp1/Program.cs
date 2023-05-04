cc8e806369d0ff6562a189285e4909f428c7a424
Nathan <nathan@domain.local>
2023-05-04 08:57:19 -0400

    Code review/refactor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,25 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Linq;
+﻿using System.Linq;
 
 string sentence = "Hello, World! The world we live in is Earth. Say hello whenever on world.";
+sentence = sentence.Replace(",", "").Replace("!", "").ToLower();
 var words = sentence.Split(' ');
 
-Dictionary<string, int> dictionary = new Dictionary<string, int>();
-foreach (var orginalWord in words)
+// used new C# syntax for instantiating the dictionary object
+Dictionary<string, int> dictionary = new();
+foreach (var word in words)
 {
-    var lowercaseOrginalWord = orginalWord.ToLower().Replace(",", "").Replace("!", "");
     int numberOfOccurances = 0;
-    foreach (var word in words)
+    foreach (var nestedWord in words)
     {
-        var lowercaseWord = word.ToLower().Replace(",", "").Replace("!", "");
-        if (lowercaseWord == lowercaseOrginalWord)
+        if (nestedWord == word)
         {
             numberOfOccurances++;   
         }
     }
-    dictionary[lowercaseOrginalWord] = numberOfOccurances;
+    dictionary[word] = numberOfOccurances;
 }
 dictionary = dictionary.OrderByDescending(pair => pair.Value).ToDictionary(x => x.Key, y => y.Value);
+
 Console.WriteLine($"Sentence: {sentence}");
 Console.WriteLine(String.Concat("Word with the most occurances: " + dictionary.ElementAt(0).Key));
 Console.WriteLine(String.Concat("Word with the second most occurances: " + dictionary.ElementAt(1).Key));
